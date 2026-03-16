01a4e09e05ef11304375e02ffa66f6409d1132e0
natejacobs1 <adarshsk128@gmail.com>
2026-03-16 23:40:59 +0530

    2nd Day progress

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import React from "react";
+const Pizza = (props) => {
+     return React.createElement("div",{},[
+         React.createElement("h1", {}, props.name),
+         React.createElement("p", {}, props.description),
+         ]
+     );
+};
+
 
 const App = () => {
      return React.createElement(
          "div",
          {},
-         React.createElement("h1", {}, "Padre Genos")
-     )
-}
+         [
+         React.createElement("h1", {}, "Padre Genos"),
+         React.createElement(Pizza, {
+              name: "Chikku pizza",
+              description: "Just like pineapple",
+         }),
+         React.createElement(Pizza),
+         React.createElement(Pizza),
+         React.createElement(Pizza),
+         React.createElement(Pizza),
+         ]
+
+     );
+};
 
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
